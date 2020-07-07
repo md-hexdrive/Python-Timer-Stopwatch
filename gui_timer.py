3686a8a1d333c1b1a726b119c1b77d77ce76338d
md-hexdrive <pdotzert7@gmail.com>
2020-07-07 12:21:01 -0400

    Update gui_timer.py
    Fix timing code to provide proper countdown and user control over countdown (pausing/playing the timer).

--- gui_timer.py
+++ gui_timer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font
+from tkinter import ttk
 
 import datetime
 import time
@@ -13,7 +14,7 @@ class Gui_Timer:
         global timer_counter
         timer_counter += 1
         self.timer_id = timer_counter
-        self.my_timer = timer.Timer(mins=2, secs = 10)
+        self.my_timer = timer.Timer(mins=2)#, secs = 10)
         
         self.root = root
         self.root.title("Timer " + str(self.timer_id))
@@ -31,8 +32,9 @@ class Gui_Timer:
         
         self.button_frame = tk.Frame(self.root)
         self.start_button = tk.Button(self.button_frame, text="Start/Stop", font=self.button_font,
-                                      command=self.start_timer)
-        self.reset_button = tk.Button(self.button_frame, text="Reset", font=self.button_font)
+                                      command=self.start_stop_timer)
+        self.reset_button = tk.Button(self.button_frame, text="Reset", font=self.button_font,
+                                      command=self.reset_timer)
         self.change_time_button = tk.Button(self.button_frame, text="Change Time",
                                             font=self.button_font)
         self.new_timer_button = tk.Button(self.button_frame, text="+", font=self.button_font,
@@ -44,10 +46,11 @@ class Gui_Timer:
         self.change_time_button.grid(row=0, column=2, padx=3, pady=5)
         self.new_timer_button.grid(row=0, column=3, padx=3, pady=5)
         
+        self.timer_running = False
         self.root.mainloop()
     
     def start_stop_timer(self):
-        if self.my_timer.is_done():
+        if not self.timer_running:
             self.start_timer()
         else:
             self.stop_timer()
@@ -56,10 +59,13 @@ class Gui_Timer:
         #self.timer_thread = threading.Thread(target=self.my_timer.start)
         
         #self.timer_thread.start()
+        self.timer_running = True
         self.root.after(1000, self.update_countdown)
     
     def stop_timer(self):
-        self.my_timer.stop()
+        print("Stopping timer")
+        self.timer_running = False
+        #self.my_timer.done = True
     
     def reset_timer(self):
         self.my_timer.reset()
@@ -69,16 +75,18 @@ class Gui_Timer:
         pass
     
     def create_new_timer(self):
-        gui_timer = Gui_Timer()
+        gui_timer = Gui_Timer(self.root)
         
     def update_countdown(self):
-        self.my_timer.decrement()
-        self.countdown_time.set(self.my_timer.now())
-        if self.my_timer.hours == 0 and self.my_timer.minutes == 0 and self.my_timer.seconds == 0 or self.my_timer.is_done():
-            self.my_timer.timer_done(False)
-            return
-        else:
-            self.root.after(1000, self.update_countdown)
+        if self.timer_running:
+            self.my_timer.decrement()
+            self.countdown_time.set(self.my_timer.now())
+            if self.my_timer.hours == 0 and self.my_timer.minutes == 0 and self.my_timer.seconds == 0 or self.my_timer.is_done():
+                self.timer_running = False
+                self.my_timer.timer_done(can_play_alarm=False)
+                return
+            else:
+                self.root.after(1000, self.update_countdown)
 
 
 if __name__ == "__main__":
