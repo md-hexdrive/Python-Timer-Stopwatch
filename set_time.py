d2fb6fdf9f43bbdf47833f63e47428dc8985975f
md-hexdrive <pdotzert7@gmail.com>
2020-07-07 12:18:16 -0400

    Create set_time.py
    Adding the ability to set the countdown's length in a gui window.

---
+++ set_time.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+import counter
+import gui_timer
+
+class Set_Time(tk.Toplevel):
+    def __init__(self, title="Set Time"):
+        super().__init__()
+        self.title(title)
+        
+        style = ttk.Style(self)
+        
+        style.configure("mystyle.TButton", highlightthickness=0, bd=0, font=('Arial', 24))
+        style.configure("mystyle.TCombobox.Heading", font=('Arial', 48))
+        
+        time_font = font.Font(family='Arial', size=48, weight='bold')
+        
+        self.hours_str = tk.StringVar()
+        self.mins_str = tk.StringVar()
+        self.secs_str = tk.StringVar()
+        
+        hours_combo = ttk.Combobox(self, width=2, values=tuple(range(0,100)),
+                                   textvariable=self.hours_str, style="mystyle.TCombobox")
+        minutes_combo = ttk.Combobox(self, width=2, values=tuple(range(0,60)),
+                                   textvariable=self.mins_str, style="mystyle.TCombobox")
+        seconds_combo = ttk.Combobox(self, width=2, values=tuple(range(0,60)),
+                                   textvariable=self.secs_str, style="mystyle.TCombobox")
+        
+        
+        hours_combo.grid(row=1, column=0, padx=5, pady=5)
+        minutes_combo.grid(row=1, column=2, padx=5, pady=5)
+        seconds_combo.grid(row=1, column=4, padx=5, pady=5)
+        
+        colon_label = ttk.Label(self, text=":")#, font=time_font)
+        colon_label2 = ttk.Label(self, text=":")#, font=time_font)
+        colon_label.grid(row=1, column=1, padx=5, pady=5)
+        colon_label2.grid(row=1, column=3, padx=5, pady=5)
+        
+        
+        
+        set_time_button = ttk.Button(self, text="Set Time", style="mystyle.TButton")#, font=font.Font(family='Arial', size='24'))
+        set_time_button.grid(row=2, column=3, columnspan=2, padx=5, pady=5)
+
+if __name__=='__main__':
+    Set_Time()
