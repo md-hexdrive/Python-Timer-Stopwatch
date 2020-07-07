3f7d16d0a4e2660b2ece53d8f433f09affb2d7ea
md-hexdrive <pdotzert7@gmail.com>
2020-07-07 12:26:07 -0400

    Update counter.py
    A counter instance can now be created from a wider range of input formats (a string, a tuple/list, and a list/tuple of strings containing the length of time to set run the countdown for).
    
    Tested the new timer creation capabilities.

--- counter.py
+++ counter.py
@@ -15,14 +15,41 @@ TODO: add alarms to the program
 TODO: allow the user to tell the program to perform a user-defined action (run a program, call a function, etc.) when at specific timer/alarm is done (or even reaches a certain point in time)
 
 """
-class Counter:
-    def __init__(self, hrs = 0, mins = 0, secs = 0):
-        self.hours = hrs
-        self.minutes = mins
-        self.seconds = secs
-        self.done = False
+
+def split_time_str(time_str):
+    times = time_str.split(':')
+    return times
+
+def strarr_to_time(time_str_arr):
+    time_arr = []
+    for s in time_str_arr:
+        time_arr.append(int(s))
+    return time_arr
         
-        self.start_time = (hrs, mins, secs)
+class Counter:
+    def __init__(self, hours = 0, mins = 0, secs = 0, compound_time = ()):
+        
+        
+        if compound_time != None and (type(compound_time) == type(list()) or type(compound_time) == type(tuple())) and len(compound_time) == 3:
+            if type(compound_time[0]) == type(str()):
+                compound_time = strarr_to_time(compound_time)
+            
+            self.hours = compound_time[0]
+            self.minutes = compound_time[1]
+            self.seconds = compound_time[2]
+        elif compound_time != None and type(compound_time) == type(str()) and compound_time != "":
+            this_time = strarr_to_time(split_time_str(compound_time))
+            self.hours = this_time[0]
+            self.minutes = this_time[1]
+            self.seconds = this_time[2]
+        
+        else:
+            self.hours = hours
+            self.minutes = mins
+            self.seconds = secs
+            
+        self.done = False
+        self.start_time = (self.hours, self.minutes, self.seconds)
     
     def set_time(self, hours, minutes, seconds):
         self.hours = hours
@@ -73,6 +100,7 @@ class Counter:
     def reset(self):
         self.done = False
         self.hours, self.minutes, self.seconds = self.start_time
+        
     def print_time(self):
         print(self.now())
         
@@ -102,12 +130,12 @@ class FormatTime:
         return hr_str + ":" + min_str + ":" + sec_str
 
 if __name__ == "__main__":
-    counter = Counter(3, 59, 50)
-    print(counter.now())
+    counter = Counter(compound_time=("5", "24", "4"))
+    print(counter)
     
     for i in range(20):
         counter.increment()
-        print(counter.now())
+        print(counter)
     
     counter.reset()
-    print("Timer Reset: ", counter.now())
+    print("Timer Reset: ", counter)
